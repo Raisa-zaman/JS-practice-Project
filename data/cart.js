a26b16ae639b5fc7f2a6bc8181dc59e405118cfc
Raisa zaman <raisazaman614@gmail.com>
2025-12-24 00:14:53 +0600

    generating html fororder summmary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export  const cart = [];
+export  const cart = [{
+  productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+  quantity : 2
+},{
+  productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+ quantity: 1
+}];
 export function addToCart(productId){
     const quantity = Number(document.querySelector(`.js-select-quantity-${productId}`).value);
 
@@ -18,12 +24,3 @@ if(matchingItem){
  }
 }
 
-export function updateCartQuantity(){
-let cartQuantity = 0;
-
-cart.forEach((cartItem)=>{
- cartQuantity  += cartItem.quantity;
-});
-
-document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-}
